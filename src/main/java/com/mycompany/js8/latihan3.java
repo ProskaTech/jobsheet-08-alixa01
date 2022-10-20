86bfdbf8586967ec17967b114526d1cd405d3085
alixa <94752755+alixa01@users.noreply.github.com>
2022-10-20 19:10:08 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/js8/latihan3.java
@@ -0,0 +1,17 @@
+// Created by 21343040_alixa arivya tofer
+package com.mycompany.js8;
+
+/**
+ *
+ * @author HP
+ */
+public class latihan3 {
+    public static void main(String args[]){
+        String[][] dogs = {{"Teryy", "Brown"},
+                          {"Kristin", "White"},
+                          {"Toby", "Gray"},
+                          {"Fido", "Black"}
+                        };
+        System.out.println(dogs[0][1]);
+        }
+}
---
+++ src/main/java/com/mycompany/js8/tugas1.java
@@ -0,0 +1,15 @@
+// Created by 21343040_alixa arivya tofer
+package com.mycompany.js8;
+
+/**
+ *
+ * @author HP
+ */
+public class tugas1 {
+    public static void main(String args[]){
+        String days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        for (int i = 0; i < days.length; i++){
+            System.out.println(days[i]);
+        }
+    }
+}
